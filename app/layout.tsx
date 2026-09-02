cebcc4629705e2ff9f15a39dfc8130580ee5babc
ruswanhidayat <62171225+ruswanhidayat@users.noreply.github.com>
2026-09-02 14:34:06 +0700

    fixing error

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,4 @@ export default function RootLayout({
       <body>{children}</body>
     </html>
   );
-}import "./globals.css";
-
-export const metadata = {
-  title: "Kas",
-  description: "Aplikasi pencatatan pembayaran kas",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="id">
-      <body>{children}</body>
-    </html>
-  );
 }
